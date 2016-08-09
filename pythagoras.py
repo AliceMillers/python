4022eb903308ebfa17a3ebe395bc4092c9f5dd6e
AliceMillers <Panterka13@yandex.ru>
2016-08-09 21:28:00 +0300

    New task_commit_1

--- pythagoras.py
+++ pythagoras.py
@@ -3,11 +3,11 @@ import math
 a = int(input("Tell A"))
 b = int(input("Tell B"))
 
-if (a > 0 and b > 0):
-    def pythagoras(a, b):
-        value = math.sqrt(a * a + b * b)
-        return value
-    result = pythagoras(a, b)
-    print(result)
-else:
-    print('-1')
+def pythagoras(a, b):
+        if (a > 0 and b > 0):
+            value = math.sqrt(a * a + b * b)
+            return value
+        else:
+            return -1
+result = pythagoras(a, b)
+print(result)
