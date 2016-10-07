02f488652d081eeee710a73df9094999b21b38bc
AliceMillers <Panterka13@yandex.ru>
2016-10-07 17:48:18 +0300

    1

---
+++ pig_latin_translatore.py
@@ -0,0 +1,13 @@
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+# Mare sure that new word consists letters and doesn't consist numeric
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word [0]
+    new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
+    print (new_word)
+else:
+    print ('empty')
