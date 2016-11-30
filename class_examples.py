d24df2053c98e078a4848178fa2b581b2924681d
AliceMillers <Panterka13@yandex.ru>
2016-11-30 15:49:05 +0100

    Examples Classes 3

--- class_examples.py
+++ class_examples.py
@@ -30,3 +30,30 @@ class Animal(object):
         print (self.age)
 hippo = Animal("Ostin", 7)
 print (hippo.name, hippo.age)
+
+#shoppincart
+class ShoppingCart(object):
+    items_in_cart = {}
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    def add_item(self, product, price):
+        """Add product to the cart."""
+        if not product in self.items_in_cart:
+            self.items_in_cart[product] = price
+            print (product + " added.")
+        else:
+            print (product + " is already in the cart.")
+
+    def remove_item(self, product):
+        """Remove product from the cart."""
+        if product in self.items_in_cart:
+            del self.items_in_cart[product]
+            print (product + " removed.")
+        else:
+            print (product + " is not in the cart.")
+my_cart = ShoppingCart("Patrick")
+my_cart.add_item("Milk", 15.9)
+my_cart.add_item("Bread", 6)
+my_cart.add_item("Vodka", 22.5)
+my_cart.remove_item("Vodka")
