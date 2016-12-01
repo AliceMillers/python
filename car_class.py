8843487de465d42b57bfbd680cde8f23ed472a0d
AliceMillers <Panterka13@yandex.ru>
2016-12-01 20:04:44 +0100

    Car Class 4

--- car_class.py
+++ car_class.py
@@ -39,3 +39,14 @@ my_car = ElectricCar("Volvo", "black", 50, "molten salt")
 print (my_car.condition)
 my_car.drive_car()
 print (my_car.condition)
+
+#third example
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def __repr__(self):
+        return "(%s, %s, %s)" % (self.x, self.y, self.z)
+my_point = Point3D(1, 2, 3)
+print (my_point)
