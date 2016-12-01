749b7bc871ba7512781fb3ad1a22c9d2a1e5c8cf
AliceMillers <Panterka13@yandex.ru>
2016-12-01 19:21:56 +0100

    Car Class 2

--- car_class.py
+++ car_class.py
@@ -4,9 +4,10 @@ class Car(object):
     def __init__(self, model, color, mpg):
         self.model = model
         self.color = color
-        self.mpg = mpg
+        self.mpg   = mpg
+    def display_car(self):
+        return "This is a %s %s with %s MPG." % ( self.color, self.model, str(self.mpg))
+
 my_car = Car("DeLorean", "silver", 88)
 print (my_car.condition)
-print (my_car.model)
-print (my_car.color)
-print (my_car.mpg)
+print (my_car.display_car())
