a0f43a85507c7e943c653265a7a001227be9c76a
AliceMillers <Panterka13@yandex.ru>
2016-12-01 19:52:27 +0100

    Car Class 4

--- car_class.py
+++ car_class.py
@@ -26,5 +26,16 @@ class Car(object):
 
 my_car = Car("DeLorean", "silver", 88)
 print (my_car.condition)
-print (my_car.drive_car())
+my_car.drive_car()
+print (my_car.condition)
+
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        super(ElectricCar, self).__init__(model, color, mpg)
+        self.battery_type = battery_type
+    def drive_car(self):
+        self.condition = "like new"
+my_car = ElectricCar("Volvo", "black", 50, "molten salt")
+print (my_car.condition)
+my_car.drive_car()
 print (my_car.condition)
