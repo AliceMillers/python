5919c115c5c706ad60e475e3eb152f081baea4b8
Iulia <Panterka13@yandex.ru>
2016-12-09 13:09:16 +0100

    Create group.py

---
+++ group.py
@@ -0,0 +1,8 @@
+class Group(Person):
+    people = []
+    def __init__(self, person):
+        person = self.person
+    def add_person(self):
+        people = self.people + self.people[person]
+    def say_hello(self):
+        return "Hi, i'm %s. My age is %s." % ( self.age, self.name)
