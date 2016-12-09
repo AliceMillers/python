ff8f29a3ead60eb54a2eb5374b9bc44cc9d8aa90
Iulia <Panterka13@yandex.ru>
2016-12-09 13:22:27 +0100

    Update group.py

--- group.py
+++ group.py
@@ -1,8 +1,10 @@
-class Group(Person):
+class Group(object):
     people = []
-    def __init__(self, person):
-        person = self.person
+    def __init__(self, person, age, name):
+        self.person = person
+        self.age = age
+        self.name = name
     def add_person(self):
-        people = self.people + self.people[person]
+        self.people[person]
     def say_hello(self):
         return "Hi, i'm %s. My age is %s." % ( self.age, self.name)
