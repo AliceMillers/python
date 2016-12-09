1f9dc5f7134dcb1c1c98ebd3a17b7c7b201d7d26
Iulia <Panterka13@yandex.ru>
2016-12-09 13:01:30 +0100

    Update person.py

--- person.py
+++ person.py
@@ -1,3 +1,7 @@
 class Person(object):
     def __init__(self, name, age):
+        age = self.age
+        name = self.name
+    def say_hello(self):
+        return "Hi, I'm %s. My age is %s." % ( self.age, self.name)
   
