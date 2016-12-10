f218cc1ef383d77ec0c3a4bd35323eadf58b4f7c
AliceMillers <Panterka13@yandex.ru>
2016-12-10 17:18:24 +0100

    2

--- person.py
+++ person.py
@@ -4,4 +4,9 @@ class Person(object):
         self.name = name
     def say_hello(self):
         return "Hi, I'm %s. My age is %s." % ( self.age, self.name)
+    def get_age(self):
+        return self.age
+    def get_name(self):
+        return self.name
+
   
